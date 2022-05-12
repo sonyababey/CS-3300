4ebfe6e922850c859d624dd6ea45d2c0142a6e7b
Sonya <sherrer4@uccs.edu>
2022-05-12 06:19:56 +0000

    Version1

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,20 +14,15 @@ class ProjectsController < ApplicationController
 
   # GET /projects/new
   def new
-    before_action :authenticate_student! 
-
     @project = Project.new
   end
 
   # GET /projects/1/edit
   def edit
-    before_action :authenticate_student! 
   end
 
   # POST /projects or /projects.json
   def create
-    before_action :authenticate_student! 
-
     @project = Project.new(project_params)
 
     respond_to do |format|
@@ -43,7 +38,6 @@ class ProjectsController < ApplicationController
 
   # PATCH/PUT /projects/1 or /projects/1.json
   def update  
-    before_action :authenticate_student! 
     respond_to do |format|
       if @project.update(project_params)
         format.html { redirect_to project_url(@project), notice: "Project was successfully updated." }
@@ -57,7 +51,6 @@ class ProjectsController < ApplicationController
 
   # DELETE /projects/1 or /projects/1.json
   def destroy
-    before_action :authenticate_student! 
 
     @project.destroy
 
