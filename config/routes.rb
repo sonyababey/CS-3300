d3da12193def91ec57b879042a06f8724e8bbf32
Sonya <sherrer4@uccs.edu>
2022-05-12 06:16:02 +0000

    Version1

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   resources :projects
   root to: "projects#index"
 
-  devise_for :students
-  root "projects#index"
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
